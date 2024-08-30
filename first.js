dc08a9e6c06610a054f49428ab78b3d08fa44acb
Mohit Kulkarni <124132557+mohitkulkarni1999@users.noreply.github.com>
2024-08-30 20:12:09 +0530

    VAriable rules

--- first.js
+++ first.js
@@ -13,3 +13,37 @@ console.log(age);
 height = 6.3;
 
 console.log(height);
+
+
+//case sensetive
+//Variable names are case sensitive.  “a” & “A” is different.;
+
+firstname = "mohit";
+FIRSTNAME = "rohit";
+
+console.log(firstname);
+console.log(firstname);
+console.log(FIRSTNAME);
+
+//Only letters, digits, underscore( _ ) and $ is allowed. (not even space)
+//first name = "mohit"; not allowed space
+//firstname@ = "Mohit"; not allowed special characters
+
+firstname = "mohit";
+console.log(firstname);
+
+//Only a letter, underscore( _ ) or $ should be 1st character.
+
+firstname = "mohit";
+_firstname = "mohit";
+$first_name = "mohit";
+//123first_name = "mohit";//not allowed
+
+//Reserved words cannot be variable names.
+console = "mohit";//not allowed
+console.log(console);//not allowed
+Console ="Mohit";
+console.log(console);
+
+
+
