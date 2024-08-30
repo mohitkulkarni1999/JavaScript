c79365d686c6ab8ad62497a07863afbdfddf2dd2
Mohit Kulkarni <124132557+mohitkulkarni1999@users.noreply.github.com>
2024-08-30 19:56:50 +0530

    Varibles in js

---
+++ first.js
@@ -0,0 +1,15 @@
+
+//variables in js eg
+console.log("hi mohit");
+
+fullName = "Mohit kulkarni";
+
+console.log(fullName);
+
+age = 23;
+
+console.log(age);
+
+height = 6.3;
+
+console.log(height);
