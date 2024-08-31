bcda880e21aab9d0fa3ceecc3d8a03549313397e
Mohit Kulkarni <124132557+mohitkulkarni1999@users.noreply.github.com>
2024-08-31 22:21:02 +0530

    operators in java

---
+++ operatorsAndConditionalStatement.js
@@ -0,0 +1,98 @@
+//Artemetic operators +,-,/,*
+
+// let a = 2;
+// let b = 5;
+//let ans = a + b;
+// console.log(" a ", a, " b " + b)
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+
+//Modules
+// console.log("a % b =", a % b);
+
+//Exponentiation
+// console.log("a ** b =", a ** b);
+
+//Unary Operators increment/decrement
+//increment 
+//a = a + 1;
+// a++;
+
+//decement
+// a = a - 1;
+// a--;
+// console.log(a);
+
+
+//assignment operators =, +=, -=, *=, %=, **=
+
+// let a = 4;
+// let b = 5;
+// a += 4; //a = a + 4;
+// console.log(" a =", a);//8
+
+// a -= 4; //a = a - 4;
+// console.log(" a =", a);//4
+
+// a *= 4;//a = a * 4;
+// console.log(" a =", a);
+
+// a %= 4;//a = a % 4;
+// console.log(" a =", a);
+
+// a **= 4;// a = a ** 4;
+// console.log(" a =", a);
+
+
+
+//Comparision operators
+
+// let a = 4;
+// let b = "5";
+
+// console.log("a == b", a == b);
+// console.log("a == b", a != b);
+
+// let a = 5;
+// let b = "5";
+// console.log(" a===b ", a === b);
+// console.log(" a!==b ", a !== b);
+
+// let a = 5;
+// let b = 6;
+
+// console.log(" a < b", a < b);
+// console.log(" a > b", a > b);
+// console.log(" a >= b", a >= b);
+// console.log(" a <= b", a <= b);
+
+
+
+//Logical operator
+
+//&& operator
+// let a = 4;
+// let b = 3;
+
+// let con1 = a > b;
+// let con2 = a === 4;
+
+// console.log(" a < b && a===b ", con1 && con2)
+
+//OR operator
+let a = 4;
+let b = 3;
+
+let con1 = a > b;
+let con2 = a === 4;
+
+console.log(" a > b || a === b", con1 || con2)
+
+
+
+
+
+
+
