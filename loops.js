ac92fcd0d53125c771bc3cbd7a14535a9ed1169c
Mohit Kulkarni <124132557+mohitkulkarni1999@users.noreply.github.com>
2024-09-01 22:47:54 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,27 @@
+//loops
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("mohit");
+// }
+
+//calculate sum of 1 to 5
+// let n=0;
+// for (let i=1;i<=5;i++){
+//     n=n+i;
+// }
+// console.log(n)
+
+//while loop
+
+// let i=1;
+// while(i<=5){
+//     console.log("mohit");
+//     i++;
+// }
+
+//do while loop
+let i=1;
+do{
+    console.log(i);
+    i++;
+} while (i<=5);
